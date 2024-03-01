1b266a8adbe13aef2aec26c295d27b7517090031
root <root@DESKTOP-IBN72CO>
2024-03-01 19:03:57 +0000

    Makefile

---
+++ CA1/main/daemonControl.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+
+if [ "$#" -lt 2 ]; then
+    echo "Invalid number of arguments\n"
+    exit 1
+fi
+
+if ["$0" = "start"]; then
+    echo "Starting daemon..." 
+elif ["$0" = "stop"]; then    
+    echo "Stopping daemon..."
+fi
